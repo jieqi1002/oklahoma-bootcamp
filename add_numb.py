1878a3da9b254151d1a254f9fc52486f2b697d14
jie <qijiemars@gmail.com>
2013-07-01 15:29:45 -0400

    Added module with functions

---
+++ add_numb.py
@@ -0,0 +1,12 @@
+# This has been authorized by but who knows will contribute 
+from adding import add_two, add_three
+ 
+
+def main():
+  print ("2 + 2 + 2 =", add_three(2, 2, 2))
+  print ("2 + 3 = ", add_two(2, 3))
+  
+if __name__ == "__main__":
+  main()
+
+
